312fea450e1c0109440066a468e08aab66edb60b
Aderinsola Akintilo <aderinsola.akintilo@nike.com>
2017-04-18 17:12:55 -0700

    added some styling to cards || centered information || changed color styles

--- build/js/templates.js
+++ build/js/templates.js
@@ -7,29 +7,11 @@ __p += '<div class="l-main">\n    <div class="l-header"></div>\n    <div class="
 }
 return __p
 },
-"views/common/compositeView/compositeView": function(obj) {
-obj || (obj = {});
-var __t, __p = '', __e = _.escape;
-with (obj) {
-__p += '<div class="cv__container"></div>';
-
-}
-return __p
-},
-"views/common/compositeView/compositeView_item": function(obj) {
-obj || (obj = {});
-var __t, __p = '', __e = _.escape;
-with (obj) {
-__p += '<div class="">\n\tItem\n</div>';
-
-}
-return __p
-},
 "views/components/card/card": function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="card__header">\n\t<img src=\'' +
+__p += '<div class="card__header margin--top">\n\t<img src=\'' +
 ((__t = ( image )) == null ? '' : __t) +
 '\' alt="">\n\t<p class="card__title">\n\t\t' +
 ((__t = ( title )) == null ? '' : __t) +
@@ -47,7 +29,7 @@ __p += '<div class="card__header">\n\t<img src=\'' +
 ((__t = ( urls )) == null ? '' : __t) +
 '\n\t</p>\n\t<p class="card__openGraph">\n\t\t' +
 ((__t = ( openGraph )) == null ? '' : __t) +
-'\n\t</p>\n</div>\n\n\n<!--\n\n\t\tthis.set("message", Post.message);\n\n\t\t\t// Get the title\n\t\t\tthis.set("title", Post.title);\n\n\t\t\t// Get the creation time \n\t\t\tthis.set("created_time", Post.created_time);\n\n\t\t\t// Get who it was from \n\t\t\tthis.set("from", Post.from);\n\n\t\t\t// Get how many likes it had\n\t\t\tthis.set("likes", Post.likes);\n\n\t\t\t// Get the description \n\t\t\tthis.set("description", Post.description);\n\n\t\t\t// Get and save the urls just in case we need this info \n\t\t\tthis.set("urls", Post.urls);\n\n\t\t\t// Get and save the link ( if it has one )\n\t\t\tthis.set("link", Post.link);\n\n\t\t\t// Get the image \n\t\t\tthis.set("image", Post.image);\n\n\t\t\t// Get the open graph data if it has it\n\t\t\tthis.set("openGraph", Post.openGraph);-->';
+'\n\t</p>\n</div>\n\n\n<!--\n\n\t\tthis.set("message", Post.message);\n\n\t\t\t// Get the title\n\t\t\tthis.set("title", Post.title);\n\n\t\t\t// Get the creation time\n\t\t\tthis.set("created_time", Post.created_time);\n\n\t\t\t// Get who it was from\n\t\t\tthis.set("from", Post.from);\n\n\t\t\t// Get how many likes it had\n\t\t\tthis.set("likes", Post.likes);\n\n\t\t\t// Get the description\n\t\t\tthis.set("description", Post.description);\n\n\t\t\t// Get and save the urls just in case we need this info\n\t\t\tthis.set("urls", Post.urls);\n\n\t\t\t// Get and save the link ( if it has one )\n\t\t\tthis.set("link", Post.link);\n\n\t\t\t// Get the image\n\t\t\tthis.set("image", Post.image);\n\n\t\t\t// Get the open graph data if it has it\n\t\t\tthis.set("openGraph", Post.openGraph);-->\n';
 
 }
 return __p
@@ -57,6 +39,24 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
 __p += '<div class="card__container">\n    \n</div>';
+
+}
+return __p
+},
+"views/common/compositeView/compositeView": function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="cv__container"></div>';
+
+}
+return __p
+},
+"views/common/compositeView/compositeView_item": function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="">\n\tItem\n</div>';
 
 }
 return __p
